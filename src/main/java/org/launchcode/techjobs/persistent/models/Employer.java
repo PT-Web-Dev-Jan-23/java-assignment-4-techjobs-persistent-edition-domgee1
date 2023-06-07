3998500e923eef543ae60ded0a8e4961c7dbe24b
domgee1 <dominicgoldman@yahoo.com>
2023-06-07 18:50:07 -0500

    working on it

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,15 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Employer extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
 
+@Entity
+public class Employer extends AbstractEntity {
+@NotBlank(message = "Location must be provided")
+    private String location;
+public Employer() {}
+    public Employer(String location) {
+        this.location = location;
+    }
 
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+@Entity
 public class Skill extends AbstractEntity {
+    @NotBlank(message = "Description is required")
+    @Size(min = 3, max = 400)
+    private String description;
 
 }
